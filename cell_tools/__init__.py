31c2462db28141dd20a9e294631e95cf87236d5a
mvinyard <mvinyard@broadinstitute.org>
2021-10-30 00:59:23 +0000

    org

--- cell_tools/__init__.py
+++ cell_tools/__init__.py
@@ -8,4 +8,7 @@ __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
 from . import _ATAC as atac
 from . import _RNA as rna
-from . import _Multiome as multi
+from . import _multiome as multi
+from . import _datasets as data
+from . import _readwrite as io
+from . import _utilities as util
