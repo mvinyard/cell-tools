fc7dde48cba6b34fedff54f41ef67db6745f8622
mvinyard <mvinyard@broadinstitute.org>
2021-10-30 01:00:35 +0000

    setup modules

---
+++ cell_tools/_RNA/__init__.py
@@ -0,0 +1,14 @@
+
+# main __init__.py
+
+__module_name__ = "__init__.py"
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu",])
+
+
+from . import _ATAC as atac
+from . import _RNA as rna
+from . import _multiome as multi
+from . import _datasets as data
+from . import _readwrite as io
+from . import _utilities as util
