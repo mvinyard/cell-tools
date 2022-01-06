4713e7c87be44640e17df2e2eb71d6ce67335486
mvinyard <mvinyard@broadinstitute.org>
2022-01-06 02:55:48 +0000

    multiome feature filter func

--- cell_tools/_multiome/__init__.py
+++ cell_tools/_multiome/__init__.py
@@ -4,3 +4,5 @@
 __module_name__ = "__init__.py"
 __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["vinyard@g.harvard.edu",])
+
+from ._filter_features_multiomic import _filter_features_multiomic as filter_features_multiomic
