52d384cb5093d886672a49f27b8a5687b2cac3dc
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-01-08 18:01:56 -0700

    Update __init__.py

--- cell_tools/_general_tools/__init__.py
+++ cell_tools/_general_tools/__init__.py
@@ -7,3 +7,8 @@ __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
 from ._funcs._Harmony._HarmonizeModule import _Harmonize as Harmonize
 from ._funcs._pca import _pca_anndata as PCA
+from ._funcs._umap import _umap_anndata as UMAP
+
+# these less common, simpler APIs for PCA and UMAP can also be used directly but they are hidden to prioritize the AnnData-compatible functions
+from ._funcs._umap import _umap
+from ._funcs._pca import _pca
