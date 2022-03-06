45f297e0470a61b71d5d8967ec03c9518b3cfd8f
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-03-05 17:52:49 -0700

    Update __init__.py

--- cell_tools/_RNA/__init__.py
+++ cell_tools/_RNA/__init__.py
@@ -9,6 +9,6 @@ from ._funcs._calculate_differential_gene_expression import _calculate_different
 from ._funcs._filter_static_genes import _filter_static_genes as filter_static_genes
 from ._funcs._find_marker_genes import _find_marker_genes as id_marker_genes
 
-from ._funcs._remove_correlated_genes_adata import _remove_correlated_genes_adata as remove_correlated_genes
+# from ._funcs._remove_correlated_genes_adata import _remove_correlated_genes_adata as remove_correlated_genes
 
 from ._funcs._filter_cells_by_gene_set import _GeneSetFiltering as gene_set_filtering
