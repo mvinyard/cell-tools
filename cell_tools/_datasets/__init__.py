e45680d22aabb0f3e89e715e5189fabe54befa38
mvinyard <mvinyard.ai@gmail.com>
2023-10-10 23:08:39 +0000

    clean slate

--- cell_tools/_datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-
-# _datasets __init__.py
-
-__module_name__ = "__init__.py"
-__author__ = ", ".join(["Michael E. Vinyard"])
-__email__ = ", ".join(["vinyard@g.harvard.edu",])
-
-
-from ._datasets._pbmc3k_preprocessed import _pbmc3k_preprocessed as pbmc3k_preprocessed
--- setup.py
+++ setup.py
@@ -1,51 +1,32 @@
 
-# setup.py
-
 __module_name__ = "setup.py"
 __author__ = ", ".join(["Michael E. Vinyard"])
-__email__ = ", ".join(["vinyard@g.harvard.edu",])
+__email__ = ", ".join(["mvinyard.ai@gmail.com",])
 
 
-# package imports #
-# --------------- #
+# -- import packages: ---------------------------------------------------------
+import setuptools
 import os
 import re
-from setuptools import setup
 import sys
 
 
-setup(
+setuptools.setup(
     name="cell_tools",
-    version="0.0.2",
-    python_requires=">3.7.0",
-    author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
-    author_email="mvinyard@broadinstitute.org",
+    version="0.0.1rc0",
+    python_requires=">3.9.0",
+    author="Michael E. Vinyard",
+    author_email="mvinyard.ai@gmail.com",
     url="https://github.com/mvinyard/cell-tools",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    description="cell_tools - Analysis Tools for Single-Cell Data",
-    packages=[
-        "cell_tools",
-        "cell_tools._ATAC",
-        "cell_tools._RNA",
-        "cell_tools._datasets",
-        "cell_tools._multiome",
-        "cell_tools._readwrite",
-        "cell_tools._utilities",
-    ],
+    description="Tools for working with single-cell data.",
+    packages=setuptools.find_packages(),
     install_requires=[
-        "anndata>=0.7.8",
-        "cython>=0.29.24",
-	"episcanpy>=0.3.2",
-	"nb-black>=1.0.7",
-        "pyranges>=0.0.113",
-	"licorice>=0.0.2",
-	"pydk>=0.0.4",
-	"tqdm>=4.62.3",
     ],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
